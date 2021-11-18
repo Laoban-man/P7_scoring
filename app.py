106a7fdbf8177bcb6361e59763b52075e8aceabe
Laoban-man <nicolasmichaelroux@gmail.com>
2021-11-19 00:00:08 +0100

    update

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-# import dash and bootstrap components
 import dash
 import dash_bootstrap_components as dbc
 
 
-# set app variable with dash, set external style to bootstrap theme SUPERHERO or SANDSTONE
+# set app variable with dash, set external style to bootstrap theme
 app = dash.Dash(
     __name__,
     external_stylesheets=[dbc.themes.SANDSTONE],
-    meta_tags=[
-        {"name": "viewport", "content": "width=device-width, initial-scale=1"},
-    ],
+    meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
 )
+app.title = "Baseball Historical Statistics"
 # set app server to variable for deployment
 srv = app.server
 
@@ -18,7 +16,4 @@ srv = app.server
 app.config.suppress_callback_exceptions = True
 
 # set applicaiton title
-app.title = "MLB Data Explorer"
-
-# if __name__ == "__main__":
-#    app.run_server(debug=True)
+app.title = "MLB Historical Data Visualization"
--- movie_server.py
+++
@@ -1,7 +0,0 @@
-import hug
-
-
-@hug.get(output=hug.output_format.mp4_video)
-def watch():
-    """Watch an example movie, streamed directly to you from hug"""
-    return "movie.mp4"
