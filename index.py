d2b6269ffed161279451490813fd083460596566
Laoban-man <nicolasmichaelroux@gmail.com>
2021-11-25 00:34:47 +0100

    upload file

--- index.py
+++ index.py
@@ -102,7 +102,7 @@ def uploaded_files():
 
 def file_download_link(filename):
     """Create a Plotly Dash 'A' element that downloads a file from the app."""
-    location = "/download/{}".format(urlquote(filename))
+    location = "/uploaded_data/{}".format(urlquote(filename))
     return html.A(filename, href=location)
 
 
