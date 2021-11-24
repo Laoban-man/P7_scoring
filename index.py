fcbadda11b0b15c98df4ddc22072acef3c3635b2
Laoban-man <nicolasmichaelroux@gmail.com>
2021-11-25 00:32:30 +0100

    upload file

--- index.py
+++ index.py
@@ -8,6 +8,7 @@ from layouts import (
     appMenu,
     # menuSlider,
     # explorationMenu,
+    listLayout,
     applicationLayout,
     explorationLayout,
     predictLayout,
@@ -67,7 +68,7 @@ def display_page(pathname):
             className="home",
         )
     elif pathname.endswith("/application"):
-        return upload_file, applicationLayout
+        return upload_file, listLayout, applicationLayout
     elif pathname.endswith("/exploration"):
         return explorationLayout
     elif pathname.endswith("/predict"):
@@ -80,6 +81,47 @@ def display_page(pathname):
 def index():
     layout = html.Div([nav, container])
     return layout
+
+
+def save_file(name, content):
+    """Decode and store a file uploaded with Plotly Dash."""
+    data = content.encode("utf8").split(b";base64,")[1]
+    with open(os.path.join(UPLOAD_DIRECTORY, name), "wb") as fp:
+        fp.write(base64.decodebytes(data))
+
+
+def uploaded_files():
+    """List the files in the upload directory."""
+    files = []
+    for filename in os.listdir(UPLOAD_DIRECTORY):
+        path = os.path.join(UPLOAD_DIRECTORY, filename)
+        if os.path.isfile(path):
+            files.append(filename)
+    return files
+
+
+def file_download_link(filename):
+    """Create a Plotly Dash 'A' element that downloads a file from the app."""
+    location = "/download/{}".format(urlquote(filename))
+    return html.A(filename, href=location)
+
+
+@app.callback(
+    Output("file-list", "children"),
+    [Input("upload-data", "filename"), Input("upload-data", "contents")],
+)
+def update_output(uploaded_filenames, uploaded_file_contents):
+    """Save uploaded files and regenerate the file list."""
+
+    if uploaded_filenames is not None and uploaded_file_contents is not None:
+        for name, data in zip(uploaded_filenames, uploaded_file_contents):
+            save_file(name, data)
+
+    files = uploaded_files()
+    if len(files) == 0:
+        return [html.Li("No files yet!")]
+    else:
+        return [html.Li(file_download_link(filename)) for filename in files]
 
 
 # Set layout to index function
--- layouts.py
+++ layouts.py
@@ -111,6 +111,31 @@ upload_file = html.Div(
     ]
 )
 
+listLayout = html.Div(
+    [
+        html.H1("File Browser"),
+        html.H2("Upload"),
+        dcc.Upload(
+            id="upload-data",
+            children=html.Div(["Drag and drop or click to select a file to upload."]),
+            style={
+                "width": "100%",
+                "height": "60px",
+                "lineHeight": "60px",
+                "borderWidth": "1px",
+                "borderStyle": "dashed",
+                "borderRadius": "5px",
+                "textAlign": "center",
+                "margin": "10px",
+            },
+            multiple=True,
+        ),
+        html.H2("File List"),
+        html.Ul(id="file-list"),
+    ],
+    style={"max-width": "500px"},
+)
+
 # # Menu slider used, NOT independent, MUST be used with main menu
 # menuSlider = html.Div(
 #     [
